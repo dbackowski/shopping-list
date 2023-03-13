6b42345f3a179241cc7eba04886239b25db66e68
Damian Baćkowski <damian.backowski@chargify.com>
2023-03-13 11:16:00 +0100

    Add better routing

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
+	"regexp"
 	"strings"
 )
 
@@ -18,8 +19,71 @@ type Item struct {
 	Done bool
 }
 
+type withId struct {
+	id string
+}
+
 var items = []Item{
 	{UUID: generateUUID(), Name: "Eggs x 10", Done: false},
+}
+
+func Serve(w http.ResponseWriter, r *http.Request) {
+	var h http.Handler
+	var id string
+
+	p := r.URL.Path
+	switch {
+	case match(p, "/"):
+		h = get(listItems)
+	case match(p, "/alive"):
+		h = get(alive)
+	case match(p, "/create"):
+		h = post(addItem)
+	case match(p, "/update/([^/]+)", &id):
+		h = put(withId{id}.updateItem)
+	default:
+		http.NotFound(w, r)
+		return
+	}
+	h.ServeHTTP(w, r)
+}
+
+func match(path, pattern string, vars ...*string) bool {
+	regex := regexp.MustCompile("^" + pattern + "$")
+	matches := regex.FindStringSubmatch(path)
+
+	if len(matches) <= 0 {
+		return false
+	}
+
+	for i, match := range matches[1:] {
+		*vars[i] = match
+	}
+
+	return true
+}
+
+func get(h http.HandlerFunc) http.HandlerFunc {
+	return allowMethod(h, "GET")
+}
+
+func post(h http.HandlerFunc) http.HandlerFunc {
+	return allowMethod(h, "POST")
+}
+
+func put(h http.HandlerFunc) http.HandlerFunc {
+	return allowMethod(h, "PUT")
+}
+
+func allowMethod(h http.HandlerFunc, method string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if method != r.Method {
+			w.Header().Set("Allow", method)
+			http.Error(w, "405 method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		h(w, r)
+	}
 }
 
 func alive(w http.ResponseWriter, r *http.Request) {
@@ -34,50 +98,41 @@ func listItems(w http.ResponseWriter, r *http.Request) {
 func addItem(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	if r.Method == "POST" {
-		var item Item
+	var item Item
 
-		if err := json.NewDecoder(r.Body).Decode(&item); err != nil {
-			w.WriteHeader(http.StatusUnprocessableEntity)
-			return
-		}
-
-		item.UUID = generateUUID()
-		items = append(items, item)
-		json.NewEncoder(w).Encode(item)
-	} else {
-		w.WriteHeader(http.StatusMethodNotAllowed)
+	if err := json.NewDecoder(r.Body).Decode(&item); err != nil {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		return
 	}
+
+	item.UUID = generateUUID()
+	items = append(items, item)
+	json.NewEncoder(w).Encode(item)
 }
 
-func updateItem(w http.ResponseWriter, r *http.Request) {
+func (h withId) updateItem(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	id := strings.TrimPrefix(r.URL.Path, "/update/")
 
-	if r.Method == "PUT" {
-		id := strings.TrimPrefix(r.URL.Path, "/update/")
-
-		if len(id) == 0 {
-			w.WriteHeader(http.StatusUnprocessableEntity)
-			return
-		}
-
-		for i, v := range items {
-			if v.UUID == id {
-				var item = items[i]
-
-				if err := json.NewDecoder(r.Body).Decode(&item); err != nil {
-					w.WriteHeader(http.StatusUnprocessableEntity)
-					return
-				}
-
-				items[i] = item
-			}
-		}
-
-		json.NewEncoder(w).Encode(items)
-	} else {
-		w.WriteHeader(http.StatusMethodNotAllowed)
+	if len(id) == 0 {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		return
 	}
+
+	for i, v := range items {
+		if v.UUID == id {
+			var item = items[i]
+
+			if err := json.NewDecoder(r.Body).Decode(&item); err != nil {
+				w.WriteHeader(http.StatusUnprocessableEntity)
+				return
+			}
+
+			items[i] = item
+		}
+	}
+
+	json.NewEncoder(w).Encode(items)
 }
 
 func generateUUID() string {
@@ -96,10 +151,5 @@ func main() {
 
 	fs := http.FileServer(http.Dir("./static"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fs))
-	mux.HandleFunc("/", listItems)
-	mux.HandleFunc("/alive", alive)
-	mux.HandleFunc("/create", addItem)
-	mux.HandleFunc("/update/", updateItem)
-
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	log.Fatal(http.ListenAndServe(":8080", http.HandlerFunc(Serve)))
 }
